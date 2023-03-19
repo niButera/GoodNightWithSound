149f9119e530b235e665444021e7c261260a0e9a
NiButera <nikolasxp683@gmail.com>
2023-03-18 23:51:35 -0300

    Teste dos comandos passados em sala

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//ESSE DEU CERTO
+//Console.Write("Qual seu nome? ");
+//string nome = Console.ReadLine()!;
+//Console.Write("Boa Noite,");
+//Console.Write(nome);
+//Console.WriteLine(".");
+
+//"!" É PRA CONFIRMAR Q VC SABE QUE TEM VALOR NULO VARIAVEL
+
+Console.Beep(1320, 400);
+Console.Write("Ola! Qual seu nome? ");
+string nome = Console.ReadLine ()!;
+
+Console.Beep(1650, 400);
+Console.Write("E seu sobrenome? ");
+string sobrenome= Console.ReadLine()!;
+
+Console.Beep(2475, 400);
+Console.WriteLine ($"Boa noite, {nome} {sobrenome}.");
